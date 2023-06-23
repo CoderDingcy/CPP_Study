20630a6fcbb1a92b97a9639aa10beaef675aafc3
dcy <2284858868@qq.com>
2023-06-23 09:01:58 +0800

    模版(12)

---
+++ 类模版成员函数类外实现.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+template<class T1,class T2>
+class Person
+{
+public:
+	Person(T1 name,T2 age);
+	void Show();
+	T1 m_Name;
+	T2 m_Age;
+};
+
+template<class T1,class T2>
+Person<T1, T2>::Person(T1 name,T2 age)
+{
+	m_Name = name;
+	m_Age = age;
+}
+
+template<class T1, class T2>
+void Person<T1,T2>::Show()
+{
+	cout << "ÄêÁä£º" << m_Age << "   ÐÕÃû£º" << m_Name << endl;
+}
+
+void test01()
+{
+	Person<string, int> p("ËïÎò¿Õ", 999);
+	p.Show();
+}
+
+int main()
+{
+	test01();
+	system("pause");
+}
